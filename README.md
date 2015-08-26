5582082952ca1bbf0d1c53ebad08cd3489eb3823
biplab <sarkarbiplab@gmail.com>
2015-08-27 00:54:04 +0530

    fix

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 A tool for synthesizing conceptual designs for sensors. This is based on my research work carried out at the Indian Institute of Science, Bangalore.
 
-This is version v1.0 based angular-meteor framework.
+Version v1.0 uses the angular-meteor framework.
 
--- client/routes.js
+++ client/routes.js
@@ -1,15 +1,7 @@
 var appmod = angular.module('isapPkg');
 
-appmod.config(
+appmod.config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
     function ($urlRouterProvider, $stateProvider, $locationProvider) {
         $locationProvider.html5Mode(true);
-
-        //$stateProvider.state('top', {
-        //    url: '/top',
-        //    templateUrl: 'client/top.ng.html',
-        //    controller: ''
-        //});
-        //$urlRouterProvider.otherwise('/top');
-
     }
-);
+]);
